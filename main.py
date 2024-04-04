5451d51ea51be4c80b83f70e1e3776f0e0656bc9
nikewalce <nikewalce1@gmail.com>
2024-04-04 17:05:24 +0300

    0
    Создание класса, описание первого объекта

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello')
+class Constructor:
+    #init - конструктор, self - ссылка на текущий объект, stock - запас
+    def __init__(self, stock):
+        self.stock = stock
+
+    # #del - деструктор
+    # def __del__(self):
+    #     print('del')
+
+    def Remaining_Stock(self):
+        print(f'Оставшиеся запасы на складе: {self.stock}')
+        return 1
+
+rem_stock_list = {'Hind_legs': 5, 'Front_legs': 8, 'Seat_diagonals': 5, 'Seat': 3, 'Upper_back': 3, 'Screws': 100, 'Glue':2}
+p = Constructor(rem_stock_list)
+#print(p.stock['Glue'], type(p.stock))
+print(p.Remaining_Stock())
